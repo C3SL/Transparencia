6f5282c7ed6e0924e1e6dd35b61a8d66e4fb2777
Cristian Weiland <cw14@inf.ufpr.br>
2017-02-01 10:20:02 -0200

    Improve README and create argument number check in scripts
    Signed-off-by: Cristian Weiland <cw14@inf.ufpr.br>

--- insert_data.sh
+++ insert_data.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo "Usage: $0 <year> <month> <day>"
+if [ "$#" -ne 3 ]; then
+	echo "Usage: $0 <year> <month> <day>"
+	echo "Example: $0 2016 12 01"
+	exit
+fi
 
 ./create_config.py $1 $2 $3
 ./merge_files_es.py configs/config-${1}-${2}.json
--- resumo_cadastro.sh
+++ resumo_cadastro.sh
@@ -2,41 +2,30 @@
 path=$1
 date=$2
 
+if [ "$#" -ne 2 ]; then
+	echo "Usage: $0 <path> <date>"
+	exit
+fi
+
 echo "Processing data with args = ${path} and ${date}"
 
 input="${path}${date}_Cadastro.csv"
 output="${path}${date}_Cadastro_Ufpr_Unique.csv"
 
-# Outubro 2016
-# input="Dados_Servidores/2016-10/20161031_Cadastro.csv"
-# output="Dados_Servidores/2016-10/cadastro_2016-10-31_filters_ufpr_unique.csv"
-
-# For now, this does not work. It does not create a properly CSV.
 columns="1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42"
-#columns="1,2,23,24"
-#columns="1"
 
-#cat $input | grep --binary-files=text "UNIVERSIDADE FEDERAL DO PARANA" | cut -f $columns | sort | uniq > $output
+# About this command:
+# - Sed wraps fields in double quotes.
+# - Grep removes everyone that does not work in UFPR.
+# - Cut selects the important columns.
+# - Uniq removes repeated values.
+# - Tr removes null characters (ctrl + @).
 
-# The same as above, but wrap fields in double quotes.
-#cat $input | grep --binary-files=text "UNIVERSIDADE FEDERAL DO PARANA" | cut -f $columns | sort | uniq | sed -e 's/"//g' -e 's/^\|$/"/g' -e 's/\t/"\t"/g' > $output
-# Sed wraps fields in double quotes. Grep removes everyone that does not work in UFPR. Cut selects the important columns. Uniq removes repeated values. Tr removes null characters (ctrl + @).
-#cat $input | grep --binary-files=text "UNIVERSIDADE FEDERAL DO PARANA" | cut -f $columns | sort | uniq | sed -e 's/"//g' -e 's/^\|$/"/g' -e 's/\t/"\t"/g' | tr -d '\000' > $output
-
-# Parece funcionar, mas pra todas as Universidades
+# Get data from all universities.
 #cat $input | egrep --binary-files=text "(UNIVERSIDADE FED*|Id_SERVIDOR_PORTAL	NOME)" | sed -e 's/"//g' -e 's/^\|$/"/g' -e 's/\t/"\t"/g' | tr -d '\000' > $output
 
-#cat $input | egrep --binary-files=text "(UNIVERSIDADE FEDERAL DO PARANA|Id_SERVIDOR_PORTAL	NOME)" | sed -e 's/"//g' -e 's/^\|$/"/g' -e 's/\t/"\t"/g' | tr -d '\000' > $output
-#Mesmo que de cima, mas sem wrapar com "
-cat $input | egrep --binary-files=text "(UNIVERSIDADE FEDERAL DO PARANA|Id_SERVIDOR_PORTAL	NOME)" | tr -d '\000' > $output
+# Get data only from UFPR, and wraps it in double quotes (").
+# cat $input | egrep --binary-files=text "(UNIVERSIDADE FEDERAL DO PARANA|Id_SERVIDOR_PORTAL	NOME)" | sed -e 's/"//g' -e 's/^\|$/"/g' -e 's/\t/"\t"/g' | tr -d '\000' > $output
 
-# +--------+---------------------+--------------------------------+
-# | Column | Contains            | Example                        |
-# +--------+---------------------+--------------------------------+
-# | 1      | Id_SERVIDOR_PORTAL  | 1000021                        |
-# | 2      | NOME                | MARIA AUREA DOS SANTOS RIBEIRO |
-# | 24     | COD_ORG_LOTACAO     | 26241                          |
-# | 25     | ORG_LOTACAO         | UNIVERSIDADE FEDERAL DO PARANA |
-# | 29     | UORG_EXERCICIO      | BL - DEPARTAMENTO DE GENETICA  | Parece que é a coluna 23 na verdade...
-# | 33     | JORNADA_DE_TRABALHO | 40 HORAS SEMANAIS              |
-# +--------+---------------------+--------------------------------+
+# Same as above, but does not wrap data in double quotes (").
+cat $input | egrep --binary-files=text "(UNIVERSIDADE FEDERAL DO PARANA|Id_SERVIDOR_PORTAL	NOME)" | tr -d '\000' > $output

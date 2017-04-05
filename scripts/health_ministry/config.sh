a19cac0ac9c7e425f8a7d4ced4c5902e1e0f4043
Cristian Weiland <cw14@inf.ufpr.br>
2017-04-05 10:36:33 -0300

    Issue #38: Include scripts to insert data from health ministry
    Signed-off-by: Cristian Weiland <cw14@inf.ufpr.br>

---
+++ scripts/health_ministry/config.sh
@@ -0,0 +1,13 @@
+# This file only contains some config variables:
+
+# Index prefix: The prefix of the index in elasticsearch. Ex: gastos
+
+index="ms-gastos-pagamentos"
+
+# Filter: A string that will be used to 'egrep' the data obtained from Portal Transparência.
+
+filter="MINISTERIO DA SAUDE"
+
+# Host: ElasticSearch's host. Ex: "localhost"
+
+host="localhost"
---
+++ scripts/health_ministry/resume_health_ministry.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# WARNING: This script should not be called directly. Look at 'insert_health_ministry.sh' before calling this script.
+
+# Input: First parameter is the path to data files and the second one is the date in the name of the files. Data files can be found in: http://transparencia.gov.br/downloads/mensal.asp?c=GastosDiretos
+# Example: ./resume_health_ministry.sh ./tmp_2016-11 2016-11 "MINISTERIO DA SAUDE"
+
+# Output: A CSV file in folder processed, filtering the data to get only relevant data (in our case, from UFPR).
+
+if [ "$#" -ne 3 ]; then
+	echo "Usage: $0 <path> <date> <filter>"
+	exit
+fi
+
+# Path example: ./tmp_2016-11
+path=$1
+# Date example: 2016-11
+date=$2
+# Filter example: "MINISTERIO DA SAUDE"
+filter=$3
+# dateWithoutHyphen example: 201611
+dateWithoutHyphen=${date//-}
+
+input="${path}/${dateWithoutHyphen}_GastosDiretos.csv"
+output="${path}/${dateWithoutHyphen}.csv"
+
+# About this command:
+# - Grep removes unnecessary data.
+# - Tr removes null characters (ctrl + @).
+
+cat "$input" | egrep --binary-files=text "$filter" | tr -d '\000' > "$output"

b8f3184995b07727a4e867d35771bed03a0bdf1d
Cristian Weiland <cw14@inf.ufpr.br>
2017-04-19 10:47:09 -0300

    Issue #33: Travel Allowance: ready to add entire ministry.
    Signed-off-by: Cristian Weiland <cw14@inf.ufpr.br>

--- scripts/travel_allowances/resume_travel_allowance.sh
+++ scripts/travel_allowances/resume_travel_allowance.sh
@@ -1,34 +1,24 @@
 #!/bin/bash
 
-# WARNING: This script should not be called directly. Look at 'insert_travel_allowance.sh' before calling this script.
+input="201612_GastosDiretos.csv"
 
-# Input: First parameter is the path to data files and the second one is the date in the name of the files. Data files can be found in: http://transparencia.gov.br/downloads/mensal.asp?c=Diarias
-# Example: ./resume_travel_allowance.sh ../../data/travel_allowance/ 2016-11
+if [ "$#" -ne 4 ]; then
+    echo "Usage $0 <path> <date> <filter> <column-id>"
+    echo "Example: $0 ./tmp_201612 201612 MEC 2"
+    exit
+fi
 
-# Output: A CSV file in folder processed, filtering the data to get only relevant data (in our case, from UFPR).
-
-# Path example: ../../data/travel_allowance/
 path=$1
-# Date example: 2016-11
 date=$2
-# dateWithoutHyphen example: 201611
-dateWithoutHyphen=${date//-}
 filter=$3
+columnId=$4
 
-if [ "$#" -ne 3 ]; then
-	echo "Usage: $0 <path> <date> <filter>"
-	exit
-fi
+dateWithoutHyphen=${date//-}
+cmd="\$$columnId == \"${filter}\""
 
-input="${path}${date}/${dateWithoutHyphen}_Diarias.csv"
-output="${path}processed/${dateWithoutHyphen}.csv"
+# Input will probably look like: ./tmp_201612/201612_GastosDiretos.csv
+input="${path}/${dateWithoutHyphen}_Diarias.csv"
+# Output will probably look like: ./tmp_201612/201612.csv
+output="${path}/${dateWithoutHyphen}.csv"
 
-if [ ! -d "${path}processsed" ]; then
-    mkdir -p "${path}processed"
-fi
-
-# About this command:
-# - Grep removes irrelevant data. -w option forces to match the whole word, to avoid "UNIVERSIDADE FEDERAL DO PARA" from matching with "UNIVERSIDADE FEDERAL DO PARANA"
-# - Tr removes null characters (ctrl + @).
-
-cat "$input" | egrep -w --binary-files=text "$filter" | tr -d '\000' > "$output"
+cat "${input}" | awk -F $'\t' "$cmd" > "$output"
